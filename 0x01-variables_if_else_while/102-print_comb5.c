4ced6b699a9ee86177001dee7652e6f081d82a59
Favour-565 <ezehfavour16@gmail.com>
2023-06-16 20:18:16 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-    int p = 48, q = 48, y = 48, x = 49;
+	int num1, num2;
 
-    while (p < 58)
-    {
-        while (q < 58)
-        {
-            while (y < 58)
-            {
-                while (x < 58)
-                {
-                    putchar(p);
-                    putchar(q);
-                    putchar(' ');
-                    putchar(y);
-                    putchar(x);
-                    if (!(p == 57 && q == 57 && y == 57 && x == 57))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                    x++;
-                }
-                y++;
-                x = 48;
-            }
-            q++;
-            x = q + 1;
-            y = p;
-        }
-        p++;
-        q = 48;
-        y = p;
-        x = q + 1;
-    }
-    putchar('\n');
-    return (0);
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
-
